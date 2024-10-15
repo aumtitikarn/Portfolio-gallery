18bb4244badfa835bc12a1c8cd4b4304e6317153
aumtitikarn <aumzaa321@gmail.com>
2024-10-16 04:23:38 +0700

    .

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('scrolled');
     }
 });
+
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("show");
+}
+
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
